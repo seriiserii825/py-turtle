8b464af9ec55f70c48ae952e95c371d5f45b3545
serii burduja <seriiburduja@gmail.com>
2024-10-04 11:38:15 +0300

    feat: color square

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from turtle import Turtle, Screen
 
 tim = Turtle()
-
-for _ in range(4):
-    tim.forward(100)
+colors = ['red', 'blue', 'green', 'yellow', 'purple', 'orange', 'black', 'pink', 'brown', 'lime']
+for i in range(4):
+    for k in range(10):
+        tim.color(colors[k])
+        tim.pendown()
+        tim.forward(10)
+        tim.penup()
+        tim.forward(10)
     tim.left(90)
+
+
+
 
 screen = Screen()
 screen.exitonclick()
